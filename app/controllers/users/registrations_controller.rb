84e47305951782b5f91486857da695b0428920e6
Jackrim1 <jriminton@gmail.com>
2018-04-19 17:01:16 +0100

    mid muddle over user controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,9 +46,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  #def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, :manager, keys: [:attribute])
+   #end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
