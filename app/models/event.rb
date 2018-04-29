d284e3eba3b859365cd6d7ffa29ee16e5f137470
jackrim1 <jriminton@gmail.com>
2018-04-29 13:35:29 +0100

    event changed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :mess, optional: true
-  serialize :recurring, Hash
-
-  def recurring=(value)
-    if RecurringSelect.is_valid_rule?(value)
-      super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
-      else
-        super(nil)
-      end
-  end
+  
 end
